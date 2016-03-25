25c73b9ccf5a9a32a1a2163b9273851d99b381b0
JungSu Kim <zzragida@gmail.com>
2016-03-25 18:41:30 +0900

    add Coursera example

---
+++ CourseraExample/src/week3/Scratch.sc
@@ -0,0 +1,11 @@
+import week3.Rational
+
+object scratch {
+  new Rational(1, 2)
+
+  def error(msg: String) = throw new Error(msg)
+
+  val x = null
+  val y: String = null
+  //val z: Int = null
+}
---
+++ CourseraExample/src/week4/List.scala
@@ -0,0 +1,27 @@
+package week4
+
+trait List[T] {
+  def isEmpty: Boolean
+  def head: T
+  def tail: List[T]
+}
+
+class Cons[T](val head: T, val tail: List[T]) extends List[T] {
+  def isEmpty = false
+}
+
+class Nil[T] extends List[T] {
+  def isEmpty: Boolean = true
+  def head: Nothing = throw new NoSuchElementException("Nil.head")
+  def tail: Nothing = throw new NoSuchElementException("Nil.tail")
+}
+
+object List {
+  def singleton[T](elem: T) = new Cons[T](elem, new Nil[T])
+  singleton(1)
+  singleton(true)
+
+  // List(1, 2) = List.apply(1, 2)
+  def apply[T](x1: T, x2: T): List[T] = new Cons(x1, new Cons(x2, new Nil))
+  def apply[T]() = Nil
+}
